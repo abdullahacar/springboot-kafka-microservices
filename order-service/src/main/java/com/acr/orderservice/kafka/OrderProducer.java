d76f6542222148018e0f49e2c909b4b340c37eb3
abdullahacar <abdullahacar.aa@gmail.com>
2024-03-18 02:59:48 +0300

    kafka producer callback

--- order-service/src/main/java/com/acr/orderservice/kafka/OrderProducer.java
+++ order-service/src/main/java/com/acr/orderservice/kafka/OrderProducer.java
@@ -6,9 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class OrderProducer {
@@ -24,7 +28,7 @@ public class OrderProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(OrderEvent event){
+    public void sendMessage(OrderEvent event) {
 
         LOGGER.info(String.format("Order event => %s", event.toString()));
 
@@ -34,6 +38,35 @@ public class OrderProducer {
                 .setHeader(KafkaHeaders.TOPIC, topic.name())
                 .build();
 
-        kafkaTemplate.send(message);
+        CompletableFuture<SendResult<String, OrderEvent>> send = kafkaTemplate.send(message);
+
+        System.out.println("Message sent ! Waiting for response...");
+
+        send.whenComplete((result, err) -> {
+
+            System.out.println("Send result when complete : ");
+
+            if (err != null) {
+
+                // LOGGING
+
+                System.out.println("An error occurred {} " + err.getMessage());
+
+            } else if (result != null) {
+
+                System.out.println(result.getProducerRecord().toString());
+                System.out.println(result.getRecordMetadata().toString());
+
+            }
+
+        });
+
+
+//        SendResult<String, OrderEvent> stringOrderEventSendResult = send.join();
+//        System.out.println(stringOrderEventSendResult.getProducerRecord().topic());
+
+        System.out.println("Kafka send operation done !");
+
+
     }
 }
--- stock-service/src/main/java/com/acr/stockservice/kafka/OrderConsumer.java
+++ stock-service/src/main/java/com/acr/stockservice/kafka/OrderConsumer.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
 public class OrderConsumer {
 
@@ -13,6 +15,12 @@ public class OrderConsumer {
 
     @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(OrderEvent event) {
+
+        try {
+            Thread.sleep(Duration.ofSeconds(10).toMillis());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         LOGGER.info(String.format("Order event received in stock service => %s", event.toString()));
 
